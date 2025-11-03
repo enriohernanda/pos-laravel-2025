5625154147243730f635cce6e9d176d9414b0482
enriohernanda <enriohernanda@gmail.com>
2025-11-03 14:46:51 +0700

    update app content

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -61,7 +61,19 @@
 
     <section class="section">
       <div class="row">
-        <div class="col-lg-6">
+        <div class="col-lg-10">
+
+          <div class="card">
+            <div class="card-body">
+              {{-- <h5 class="card-title">Example Card</h5> --}}
+              {{-- <p>This is an examle page with no contrnt. You can use it as a starter for your custom pages.</p> --}}
+              @yield('content')
+            </div>
+          </div>
+
+        </div>
+
+        {{-- <div class="col-lg-6">
 
           <div class="card">
             <div class="card-body">
@@ -70,18 +82,7 @@
             </div>
           </div>
 
-        </div>
-
-        <div class="col-lg-6">
-
-          <div class="card">
-            <div class="card-body">
-              <h5 class="card-title">Example Card</h5>
-              <p>This is an examle page with no contrnt. You can use it as a starter for your custom pages.</p>
-            </div>
-          </div>
-
-        </div>
+        </div> --}}
       </div>
     </section>
 
