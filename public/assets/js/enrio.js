24137260767c36c767aaafd89219ff5f29f8b08f
enriohernanda <enriohernanda@gmail.com>
2025-11-10 08:11:42 +0700

    add cashless midtrans (masih gagal)

--- public/assets/js/enrio.js
+++ public/assets/js/enrio.js
@@ -1,92 +1,3 @@
-// // javascript variable : let, var, const
-//         // php variable = $define, const
-
-//         let nama = "enrio hernanda";
-//         var name = "bambang pamungkas";
-//         const fullname = "wahyudin kamal";
-//         // const : tetap tidak boleh merubah nilai
-//         // document.write()
-//         // console.log({
-//         //     "nama": name,
-//         //     "fullname": fullname
-//         // });
-//         // alert(nama);
-
-//         let angka1 = 10;
-//         let angka2 = 20;
-//         console.log(angka1 + angka2);
-//         console.log(angka1 - angka2);
-//         console.log(angka1 / angka2);
-//         console.log(angka1 * angka2);
-//         console.log(angka1 % angka2);
-//         console.log(angka1 ** angka2);
-
-//         // operator penugasan
-//         let x = 10;
-//         x += 5; //15
-//         console.log(x);
-
-//         // operator pembandingan
-//         //  >, <, =, ==, ===, !==
-//         let a = 1;
-//         let b = 1;
-//         if (a == b) {
-//             console.log("sama");
-//         } else {
-//             console.log("tidak sama");
-//         }
-
-//         console.log(a >= b);
-//         console.log(a <= b);
-
-//         // operator logika
-//         // &&, AND, OR, ||, !: tidak / not
-//         let umur = 20;
-//         let punyaSim = true;
-//         if (umur > 17 && punyaSim) {
-//             console.log("boleh mengemudi");
-//         } else {
-//             console.log("tidak boleh mengemudi");
-//         }
-
-//         // array : sebuah tipe data yang bisa memiliki nilai lebih dari 1
-//         let buah = ['pisang', 'salak', 'semangka'];
-//         console.log("buah di keranjang:", buah);
-//         console.log("saya mau buah", buah[1]);
-//         buah[1] = "Nanas";
-//         console.log("buah baru:", buah);
-//         buah.push('Pepaya');
-//         console.log("Buah", buah);
-//         buah.pop();
-//         console.log("Buah", buah);
-
-        // document.getElementById("product-title").innerHTML = "Data Product";
-        // // document.querySelector("#product-title");
-        // let btn = document.getElementsByClassName('category-btn');
-        // // btn[0].style.color = "red";
-        // // btn[1].style.color = "black";
-        // // btn[2].style.color = "brown";
-        // console.log("ini button", btn);
-
-//         let buttons = document.querySelectorAll(".category-btn");
-//         // buttons.forEach(function (btn){});
-//         buttons.forEach((btn) => {
-//             btn.style.color = "red";
-//             console.log(btn);
-//         })
-
-//         let card = document.getElementById("card");
-//         let h3 = document.createElement("h3");
-//         let textH3 = document.createTextNode("Selamat Datang");
-//         h3.textContent = "Selamat Datang dengan TextContent";
-
-//         let p = document.createElement("p");
-//         p.innerText = "Duarr jadi";
-//         p.textContent = "Nuall jadi";
-//         // menambahkan element didalam card
-//         card.appendChild(h3);
-//         card.appendChild(p);
-
 let currentCategory = "all";
 let products = [];
 
@@ -123,7 +34,7 @@ async function renderProducts(searchProduct = "") {
   const filtered = products.filter((p) => {
     //shorthand/ ternery
     const matchcategory =
-      currentCategory === "all" || p.category_name === currentCategory;
+      currentCategory === "all" || p.category.category_name === currentCategory;
     const matchSearch = p.product_name.toLowerCase().includes(searchProduct);
     return matchcategory && matchSearch;
   });
@@ -137,7 +48,7 @@ async function renderProducts(searchProduct = "") {
         <img src="/storage/${product.product_photo}" width="100%">
       </div>
       <div class="card-body">
-        <span class="badge bg-secondary badge-category">${product.category_name}</span>
+        <span class="badge bg-secondary badge-category">${product.category.category_name}</span>
         <h6 class="card-title mt-2 mb-2">${product.product_name}</h6>
         <p class="card-text text-primary fw-bold">Rp. ${product.product_price}</p>
       </div>
@@ -178,8 +89,9 @@ function renderCart() {
       "cart-item d-flex justify-content-between align-items-center mb-2";
     div.innerHTML = `
         <div>
-          <strong>${item.product_name}</strong>
-          <small>${item.product_price}</small>
+          <img src="/storage/${item.product_photo}" alt="test" width="50px">
+          <strong class='mb-1'>${item.product_name}</strong>
+          <small class="text-muted">Rp. ${item.product_price.toLocaleString('id-ID')}</small>
         </div>
         <div class="d-flex align-items-center">
           <button class="btn btn-outline-secondary me-2" onclick="changeQty(${item.id}, -1)">-</button>
@@ -243,28 +155,67 @@ async function processPayment() {
         alert("Cart still empty!");
         return;
     }
+
+    const modal = new bootstrap.Modal(document.getElementById('exampleModal'));
+    modal.show();
+}
+
+async function handlePayment(){
+    const paymentMethod = document.getElementById("payment_method").value;
     const order_code = document.querySelector('.orderNumber').textContent.trim();
     const subtotal = document.querySelector('#subtotal_value').value.trim();
     const tax = document.querySelector('#tax_value').value.trim();
     const grandTotal = document.querySelector('#total_value').value.trim();
-    try {
-        const res = await fetch("add-pos.php?payment", {
-            method: "POST",
-            headers: { "Content-Type": "application/json" },
-            body: JSON.stringify({ cart, order_code, subtotal, tax, grandTotal }),
-        });
-        const data = await res.json();
-        if (data.status == "success"){
-            alert("Transaction Successfully!");
-            window.location.href = "print.php";
-        }else{
-          alert("Transaction Failed", data.message);
-        }
 
-    } catch (error) {
-        alert("Upss Transaction failed!")
-        console.log("error", error);
-        die;
+    if (paymentMethod == 'cash'){
+        try {
+            const res = await fetch("/order", {
+                method: "POST",
+                headers: {
+                     "Content-Type" : "application/json" ,
+                     "X-CSRF-TOKEN" : document.querySelector('meta[name="csrf-token"]').content
+                    },
+                body: JSON.stringify({ cart, order_code, subtotal, tax, grandTotal }),
+            });
+            const data = await res.json();
+            if (data.status == "success"){
+                alert("Transaction Success!");
+                window.location.href = "order";
+            }else{
+              alert("Transaction Failed", data.message);
+            }
+
+        } catch (error) {
+            alert("Upss Transaction failed!")
+            console.log("error", error);
+            die;
+        }
+    } else if (paymentMethod == "cashless") {
+        try {
+            const res = await fetch("/cashless", {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json",
+                    "X-CSRF-TOKEN": String(document.querySelector('meta[name="csrf-token"]').content)
+                },
+                body: JSON.stringify({
+                    cart,
+                    order_code,
+                    subtotal,
+                    tax,
+                    grandTotal,
+                }),
+            });
+            const data = await res.json();
+            if (data.status == "success") {
+                window.snap.pay(data.snapToken);
+            } else {
+                alert("Transaction Failed", data.message);
+            }
+        } catch (error) {
+            alert("Upss Transaction failed!");
+            console.log("error", error);
+        }
     }
 }
 
